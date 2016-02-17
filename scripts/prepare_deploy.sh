fd4d1526017bfb63271c9b95558c26238ec3ef50
Björn Dahlgren <bjodah@gmail.com>
2016-02-17 20:36:22 +0100

    Fix deploy script

--- scripts/prepare_deploy.sh
+++ scripts/prepare_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp LICENSE doc/_build/html/
-mkdir -p deploy/branches/"${CI_BRANCH}" deploy/script_queue
-cp -r dist/* htmlcov/ doc/_build/html/ deploy/branches/"${CI_BRANCH}"/
+mkdir -p deploy/public_html/branches/"${CI_BRANCH}" deploy/script_queue
+cp -r dist/* htmlcov/ doc/_build/html/ deploy/public_html/branches/"${CI_BRANCH}"/
