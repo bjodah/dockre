0b15aa1a482a17ae4d6e7dfc1709389bd53a8de6
Björn Dahlgren <bjodah@gmail.com>
2016-08-17 17:21:49 +0200

    Tweak build.sh

--- dockre/scripts/build.sh
+++ dockre/scripts/build.sh
@@ -22,11 +22,11 @@ ABS_OUTPUT_PATH=$(unset CDPATH && cd "$2" && echo $PWD)
 # of files in our volume /output before exiting the container.
 
 cat <<'EOF' | docker run --rm -e RUNCMD="${3:-make}" -e TERM -e HOST_UID=$(id -u) -e HOST_GID=$(id -g) -v $ABS_INPUT_PATH:/input:ro -v $ABS_OUTPUT_PATH:/output ${@:5} -w /output -i ${4} bash -x
-cp -rau /input/. .
 addgroup --gid "$HOST_GID" mygroup
 adduser --disabled-password --uid "$HOST_UID" --gid "$HOST_GID" --gecos '' myuser
-chown "$HOST_UID":"$HOST_GID" /root
-su -m myuser -c "$RUNCMD"
+chown -R "$HOST_UID":"$HOST_GID" .
+cp -rau /input/. .
+su -l myuser -c "cd /output; $RUNCMD"
 BUILD_EXIT=$?
 exit $BUILD_EXIT
 EOF
